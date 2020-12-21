4b2fc56d933b03f389be5b6fa420aa2d213a627f
William Anderson <willjatlas@googlemail.com>
2020-12-21 17:24:34 +0000

    completed

--- specs/card_game_tests.py
+++ specs/card_game_tests.py
@@ -5,4 +5,20 @@ from src.card_game import CardGame
 class TestCardGame(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.card_game = CardGame()
+        self.card = Card("Hearts", 1)
+        self.card2 = Card("Spades", 7)
+
+    def test_ace_check_returns_true(self):
+        self.assertTrue(self.card_game.check_for_ace(self.card))
+    
+    def test_ace_check_returns_false(self):
+        self.assertFalse(self.card_game.check_for_ace(self.card2))
+    
+    def test_check_highest_card(self):
+        self.assertEqual(self.card2.value, self.card_game.highest_card
+                        (self.card, self.card2).value)
+    
+    def test_card_total(self):
+        cards = [self.card, self.card2]
+        self.assertEqual("You have a total of 8", self.card_game.cards_total(cards))
--- testing_task_1.py
+++ testing_task_1.py
@@ -1,23 +1,26 @@
 class CardGame:
 
-
+""" needs double equals in equality check, else 
+    needs semi colon """
   def check_for_ace(self, card):
     #if card.value = 1:
       return True
-    else
+    #else
       return False
    
-
+""" def, not dif, no comma seperating card args 
+    wrong card arg in first return, incorrect 
+    indentation below if statement"""
   # dif highest_card(self, card1 card2):
-  if card1.value > card2.value:
+  #if card1.value > card2.value:
     #return card
-  else:
-    return card2
+  #else:
+    #return card2
   
 
-
-def cards_total(self, cards):
+""" wrong indentation(whole method), total is not assigned """
+#def cards_total(self, cards):
   #total
-  for card in cards:
-    total += card.value
-    return "You have a total of" + total
+  # for card in cards:
+  #   total += card.value
+  #   return "You have a total of" + total
